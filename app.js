ae812004a7bbe31805c80ba0491001e3c5036c23
Micah Bell <micahmbell@gmail.com>
2015-08-26 11:00:30 -0600

    Section 5 - Camera Store complete.

--- app.js
+++ app.js
@@ -18,3 +18,30 @@ app.controller('Directives', function($scope) {
   $scope.repeat = [1,1,2,5,6,9,9,9];
   $scope.keys = { name: 'Liz', food: 'Cheese', movie: 'Star Wars'}
 })
+
+app.controller('CameraSale', function($scope) {
+  $scope.cameras = [
+    {
+      title: "Nikon D3100 DSLR",
+      image: "http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg",
+      rating: 3.4,
+      price: 369.99,
+      onSale: true
+    },
+    {
+      title: "Canon EOS 70D",
+      image: "http://ecx.images-amazon.com/images/I/81U00AkAUWL._SX522_.jpg",
+      rating: 2.0,
+      price: 1099.0,
+      onSale: false
+    },
+    {
+      title: "Nikon D810A",
+      image:"http://ecx.images-amazon.com/images/I/91wtXIfLl2L._SX522_.jpg",
+      rating: 4.2,
+      price: 3796.95,
+      onSale: true
+    }
+  ]
+
+})
