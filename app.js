e6a7218544a07ef2c7df666e59ac331464a493db
Micah Bell <micahmbell@gmail.com>
2015-08-25 21:22:04 -0600

    Section 5 - ng-class exercises.

--- app.js
+++ app.js
@@ -15,4 +15,6 @@ app.controller('ExercisesController', function($scope) {
 app.controller('Directives', function($scope) {
   $scope.names = ["Harry", "Ron", "Hermione", "Sirius", "Hedwig", "Tonks"];
   $scope.symbols = ["\u2660", "\u2663", "\u2665", "\u2666"];
+  $scope.repeat = [1,1,2,5,6,9,9,9];
+  $scope.keys = { name: 'Liz', food: 'Cheese', movie: 'Star Wars'}
 })
