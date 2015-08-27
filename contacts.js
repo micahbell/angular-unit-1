d4af5501ece07746f761cdc01313baf8baf66b71
Micah Bell <micahmbell@gmail.com>
2015-08-27 08:55:43 -0600

    Section 6 complete.

--- contacts.js
+++ contacts.js
@@ -2,9 +2,9 @@ var app = angular.module('ContactsApp', []);
 
 app.controller('contactsController', function($scope) {
   $scope.contacts = [
-    {name: 'Micah', email: 'micah@gmail', phone: '111-111-1111'},
-    {name: 'Jonah', email: 'jonah@gmail', phone: '222-222-2222'},
-    {name: 'T-Ray', email: 't-ray@gmail', phone: '333-333-3333'}
+    {name: 'Micah', email: 'micah@gmail', phone: '(678) 779-6282'},
+    {name: 'Jonah', email: 'jonah@gmail', phone: '(770)-983-1786'},
+    {name: 'T-Ray', email: 't-ray@gmail', phone: '(970)-819-2017'}
   ];
   $scope.addContact = function(name, email, phone) {
     $scope.name = name;
@@ -14,3 +14,5 @@ app.controller('contactsController', function($scope) {
     console.log($scope.contacts);
   };
 });
+
+// ?? Try binding name, email, and phone as properties on one newContact object rather than creating 3 different properties on the $scope
